20971842288b6d0b3533785faa09ec80be115012
dmitry-averbukh <59136584+dmitry-averbukh@users.noreply.github.com>
2021-01-09 19:07:00 +0200

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './must-have/header/header.component';
+import { FooterComponent } from './must-have/footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
